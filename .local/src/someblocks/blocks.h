8e5f5bce7007c12cab240bc6ac691669cf8635fc
MuratovAS <lexamzx@gmail.com>
2023-12-11 00:31:25 +0500

    add conf

--- .local/src/someblocks/blocks.h
+++ .local/src/someblocks/blocks.h
@@ -3,11 +3,11 @@
 // https://www.nerdfonts.com/cheat-sheet
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"󰒍 ", "ip addr | grep 'inet ' | cut -d ' ' -f6 | sed -n 2p | cut -f1 -d'/' ",	60,		0},
+	{"󰒍 ", "ip addr | grep 'inet ' | cut -d ' ' -f6 | sed -n 2p | cut -f1 -d'/' ",	15,		0},
 	{"󰍛 ", "echo \"$[100-$(vmstat 1 2|tail -1|awk '{print $15}')]\"%",	3,		0},
 	{"󰘚 ", "free -h | awk '/^Mem/ { print $3 }' | sed s/i//g",	3,		0},
-	{"", "cat /tmp/dwl-keymap",					0,		10},
-	{"", "date '+%d/%m %H:%M'",					15,		0},
+	{" ", "cat /tmp/dwl-keymap",					0,		10},
+	{"", "date '+%d/%m %H:%M'",					30,		0},
 };
 
 // sets delimeter between status commands. NULL character ('\0') means no delimeter.
